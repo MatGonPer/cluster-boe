b8b574adfc2d35eeae9e8b07f887cba1cb29789a
MatGonPer <matheushtht@gmail.com>
2025-10-24 22:04:23 -0300

    Fix: Consertado instalação do SQLX no Dockerfile

--- backend/target/debug/build/ring-c78a5fbe2cb5f45f/build_script_build-c78a5fbe2cb5f45f.d
+++ backend/target/debug/build/ring-c78a5fbe2cb5f45f/build_script_build-c78a5fbe2cb5f45f.d
@@ -1,5 +1,5 @@
-/home/matheus/projetos/rust/cluster/backend/target/debug/build/ring-c78a5fbe2cb5f45f/build_script_build-c78a5fbe2cb5f45f.d: /home/matheus/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/ring-0.17.14/build.rs
+/home/matheus/projetos/rust/cluster-boe/backend/target/debug/build/ring-c78a5fbe2cb5f45f/build_script_build-c78a5fbe2cb5f45f.d: /home/matheus/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/ring-0.17.14/build.rs
 
-/home/matheus/projetos/rust/cluster/backend/target/debug/build/ring-c78a5fbe2cb5f45f/build_script_build-c78a5fbe2cb5f45f: /home/matheus/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/ring-0.17.14/build.rs
+/home/matheus/projetos/rust/cluster-boe/backend/target/debug/build/ring-c78a5fbe2cb5f45f/build_script_build-c78a5fbe2cb5f45f: /home/matheus/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/ring-0.17.14/build.rs
 
 /home/matheus/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/ring-0.17.14/build.rs:
