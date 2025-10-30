5c99e19221ab27b2a695c94ad1d9e237e269824b
MatGonPer <matheushtht@gmail.com>
2025-10-30 02:29:28 -0300

    Fix: Trocando frontend para Typescript e inserindo regras de CORS no backend

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -7,16 +7,16 @@ mod middleware;
 use handlers::auth_handler::AppState;
 use sqlx::PgPool;
 use std::sync::Arc;
+use tower_http::cors::{CorsLayer, Any};
+use axum::http::Method;
 
 #[tokio::main]
 async fn main() {
-    //carrega as variáveis de ambiente definidas no arquivo .env
     let database_url = std::env::var("DATABASE_URL").expect("DATABASE_URL deve ser definida no .env");
     let jwt_secret = std::env::var("JWT_SECRET").expect("JWT_SECRET deve ser definia no .env");
     let token_duration_str = std::env::var("JWT_EXPIRATION_SECONDS").expect("JWT_EXPIRATION_SECONDS deve ser definida no .env");
     let token_duration_seconds = token_duration_str.parse::<i64>().expect("JWT_EXPIRATION_SECONDS deve ser um número válido de segundos");
 
-    //conecta ao banco de dados
     let db_pool = PgPool::connect(&database_url)
         .await
         .expect("Falha ao conectar ao banco de dados");
@@ -25,10 +25,15 @@ async fn main() {
         jwt_secret,
         token_duration_seconds,
     });
+    
+    let cors = CorsLayer::new()
+        .allow_origin(Any)
+        .allow_methods([Method::GET, Method::POST, Method::PATCH, Method::PUT, Method::DELETE])
+        .allow_headers(Any);
 
-    let app = router::create_router(app_state);
+    let app = router::create_router(app_state)
+        .layer(cors);
 
-    //inicia o servidor
     let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
     println!("Servidor escutando em http://localhost:3000");
     axum::serve(listener, app).await.unwrap();
